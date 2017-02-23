f918a5dc80a2735eefa8d2c38254ec30d60d591d
Jonas Einig <jonas.einig@web.de>
2017-02-23 11:55:07 +0100

    bugfix console input

--- src/SiebDesEratosthenes.java
+++ src/SiebDesEratosthenes.java
@@ -1,8 +1,8 @@
 public class SiebDesEratosthenes {
     public static void main(String[] args) {
         int n = 0;
-        if(args.length == 2){
-            n = Integer.parseInt(args[1]);
+        if(args.length == 1){
+            n = Integer.parseInt(args[0]);
         }else{//read from console
             System.out.println("Bis zu welcher oberen Grenze sollen Primzahlen berechnet werden?");
             String input = System.console().readLine();
@@ -15,6 +15,7 @@ public class SiebDesEratosthenes {
 
 
     public SiebDesEratosthenes(int n){
+        System.out.println("current n: " + n);
         //todo do sth
     }
 }
