8e721fb220436fc5a00b3c43aa088cf7d9df4055
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-02-23 12:10:17 +0100

    added scanner and refacture packague structure

--- src/main/java/SiebDesEratosthenes.java
+++ src/main/java/SiebDesEratosthenes.java
@@ -1,20 +1,27 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class SiebDesEratosthenes {
+
+    private int n;
+
     public static void main(String[] args) {
         int n = 0;
-        if(args.length == 1){
+        if(args.length == 1) {
             n = Integer.parseInt(args[0]);
-        }else{//read from console
+        } else {
+            //read from console
             System.out.println("Bis zu welcher oberen Grenze sollen Primzahlen berechnet werden?");
-            String input = System.console().readLine();
-            n = Integer.parseInt(input);
+            Scanner scanner = new Scanner(System.in);
+            n = Integer.parseInt(scanner.next());
         }
         SiebDesEratosthenes sde = new SiebDesEratosthenes(n);
     }
 
-    private int n;
 
 
-    public SiebDesEratosthenes(int n){
+    public SiebDesEratosthenes(int n) {
         System.out.println("current n: " + n);
         //todo do sth
     }
