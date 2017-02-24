d3f619bc5d2fa0a45256592ac38ea5714633c920
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-02-24 13:28:02 +0100

    some style changes

--- src/main/java/CoreOrientedMatrixMultiplicator.java
+++ src/main/java/CoreOrientedMatrixMultiplicator.java
@@ -10,68 +10,49 @@ import java.util.concurrent.TimeUnit;
  */
 public class CoreOrientedMatrixMultiplicator {
 
-    public static void main(String[] args) {
-        double [][] a = {{ 1, 2, 3, 4, 5},
-                         { 6, 7, 8, 9, 10},
-                         { 1, 2, 3, 4, 5},
-                         { 6, 7, 8, 9, 10},
-                         { 1, 2, 3, 4, 5} };
-        new CoreOrientedMatrixMultiplicator(a);
-    }
-
     private final double[][] result;
     private final double[][] matrix;
 
-    private CoreOrientedMatrixMultiplicator(double[][] matrix){
-        assert(matrix != null);
-        assert(matrix.length == matrix[0].length);
-        assert(0 < matrix.length && matrix.length < 10 );
+    public static void main(String[] args) {
+        new CoreOrientedMatrixMultiplicator(MatrixMultiplicator.testMatrix);
+    }
+
+    private CoreOrientedMatrixMultiplicator(double[][] matrix) {
+        assert (matrix != null);
+        assert (matrix.length == matrix[0].length);
+        assert (0 < matrix.length && matrix.length < 10);
 
         this.matrix = matrix;
         this.result = new double[matrix.length][matrix.length];
 
-        ArrayBlockingQueue<Runnable> tasks = new ArrayBlockingQueue<>(matrix.length * matrix.length);
+        ArrayBlockingQueue<Runnable> tasks = new ArrayBlockingQueue<Runnable>(matrix.length * matrix.length);
 
         int sysCores = Runtime.getRuntime().availableProcessors();
-        ThreadPoolExecutor tpe = new ThreadPoolExecutor( sysCores, sysCores, 0, TimeUnit.SECONDS, tasks);
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor(sysCores, sysCores, 0, TimeUnit.SECONDS, tasks);
 
         ArrayList<MatrixEntry> allEntries = new ArrayList<>();
-        for(int i = 0; i < matrix.length; i++)
-            for(int j = 0; j < matrix.length; j++)
-                allEntries.add(new MatrixEntry(i,j));
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix.length; j++)
+                allEntries.add(new MatrixEntry(i, j));
 
 
-        ArrayList< ArrayList<MatrixEntry> > workTasks = new ArrayList<>();
+        ArrayList<ArrayList<MatrixEntry>> workTasks = new ArrayList<>();
 
-        for(int i = 0 ; i < sysCores ; i++)
+        for (int i = 0; i < sysCores; i++)
             workTasks.add(new ArrayList<MatrixEntry>());
 
-        int ctr = 0 ;
+        int ctr = 0;
         for (MatrixEntry me : allEntries) {
             workTasks.get(ctr).add(me);
-            ctr = (ctr +1) % sysCores;
+            ctr = (ctr + 1) % sysCores;
         }
 
-        for(int i = 0 ; i < sysCores ; i++){
-            tpe.execute(new MultiMultiplicator( workTasks.get(i) , matrix.length));
+        for (int i = 0; i < sysCores; i++) {
+            tpe.execute(new MultiMultiplicator(workTasks.get(i), matrix.length));
         }
 
-
-
-        prettyPrintMatrix(result);
-
+        MatrixMultiplicator.prettyPrintMatrix(result);
         tpe.shutdown();
-
-
-    }
-
-    private void prettyPrintMatrix(double[][] data) {
-        for(int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++)
-                System.out.print(data[i][j] + ", ");
-
-            System.out.println();
-        }
     }
 
     private class MultiMultiplicator implements Runnable {
@@ -88,25 +69,23 @@ public class CoreOrientedMatrixMultiplicator {
         public void run() {
             for (MatrixEntry me : this.workEntries) {
                 double res = 0;
-                for(int k = 0; k < n; k++){
+                for (int k = 0; k < n; k++) {
                     res += matrix[me.fst][k] * matrix[k][me.snd];
                 }
-                synchronized (result){
+                synchronized (result) {
                     result[me.fst][me.snd] = res;
                 }
             }
         }
     }
 
-    private class MatrixEntry{
+    private class MatrixEntry {
         final int fst;
         final int snd;
-        MatrixEntry(int f, int s){
+
+        MatrixEntry(int f, int s) {
             fst = f;
             snd = s;
         }
     }
-
-
-
 }
--- src/main/java/MatrixMultiplicator.java
+++ src/main/java/MatrixMultiplicator.java
@@ -9,27 +9,33 @@ import java.util.concurrent.TimeUnit;
  */
 public class MatrixMultiplicator {
 
-    public static void main(String[] args) {
-        double [][] a = {{ 1, 2, 3, 4, 5},
-                         { 6, 7, 8, 9, 10},
-                         { 1, 2, 3, 4, 5},
-                         { 6, 7, 8, 9, 10},
-                         { 1, 2, 3, 4, 5} };
-        new MatrixMultiplicator(a);
-    }
+    static double [][] testMatrix =
+            {{ 1, 2, 3, 4, 5},
+            { 6, 7, 8, 9, 10},
+            { 1, 2, 3, 4, 5 },
+            { 6, 7, 8, 9, 10},
+            { 1, 2, 3, 4, 5 }};
 
     private final double[][] result;
     private final double[][] matrix;
 
+    public static void main(String[] args) {
+        new MatrixMultiplicator(testMatrix);
+    }
+
     private MatrixMultiplicator(double[][] matrix){
-        assert(matrix != null);
-        assert(matrix.length == matrix[0].length);
-        assert(0 < matrix.length && matrix.length < 10 );
+        try {
+            assert (matrix != null);
+            assert (matrix.length == matrix[0].length);
+            assert (0 < matrix.length && matrix.length < 10);
+        } catch (AssertionError e) {
+            System.out.println("Illegal input matrix");
+        }
 
         this.matrix = matrix;
         this.result = new double[matrix.length][matrix.length];
 
-        ArrayBlockingQueue<Runnable> tasks = new ArrayBlockingQueue<>(matrix.length * matrix.length);
+        ArrayBlockingQueue<Runnable> tasks = new ArrayBlockingQueue<Runnable>(matrix.length * matrix.length);
 
         int sysCores = Runtime.getRuntime().availableProcessors();
         ThreadPoolExecutor tpe = new ThreadPoolExecutor( sysCores, sysCores, 0, TimeUnit.SECONDS, tasks);
@@ -42,17 +48,6 @@ public class MatrixMultiplicator {
         prettyPrintMatrix(result);
 
         tpe.shutdown();
-
-
-    }
-
-    private void prettyPrintMatrix(double[][] data) {
-        for(int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++)
-                System.out.print(data[i][j] + ", ");
-
-            System.out.println();
-        }
     }
 
     private class Multiplicator implements Runnable {
@@ -79,6 +74,32 @@ public class MatrixMultiplicator {
         }
     }
 
+    // Super magic pretty print!
+    static void prettyPrintMatrix(double[][] data) {
+        String printString = "";
+        int maxValueLength = getMaxValueLength(data);
+        for(int i = 0; i < data.length; i++) {
+            printString = "| ";
+            for (int j = 0; j < data.length; j++){
+                int numberOfWhitespace = maxValueLength - (data[i][j] + "").length();
+                printString += data[i][j] + new String(new char[numberOfWhitespace]).replace("\0", " ") + " | ";
+            }
+            System.out.println(new String(new char[printString.length() - 1]).replace("\0", "-"));
+            System.out.println(printString);
+        }
+        System.out.println(new String(new char[printString.length() - 1]).replace("\0", "-"));
+    }
 
-
+    // Returns the length of the string of the greatest value of a matrix
+    private static int getMaxValueLength(double[][] data){
+        int maxValue = 0;
+        for (int i = 0; i < data.length; i++){
+            for(int j = 0; j < data[0].length; j++){
+                if (Double.toString(data[i][j]).length() > maxValue) {
+                    maxValue = Double.toString(data[i][j]).length() ;
+                }
+            }
+        }
+        return maxValue;
+    }
 }
