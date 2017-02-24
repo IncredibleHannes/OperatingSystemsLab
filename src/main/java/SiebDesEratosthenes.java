a6b6eb0632df2f8e3561d7aec2fe10d739e7ebaa
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-02-24 11:48:50 +0100

    added indetion

--- src/main/java/SiebDesEratosthenes.java
+++ src/main/java/SiebDesEratosthenes.java
@@ -1,15 +1,26 @@
 package main.java;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Scanner;
 
 public class SiebDesEratosthenes {
 
     private int n;
 
+    public SiebDesEratosthenes(int n) {
+        Worker test = new Worker(n, 2);
+        test.start();
+        for (int i = 2; i <= n + 1; i++) {
+            test.values.add(i);
+        }
+        test.values.parentStopped = true;
+
+    }
+
     public static void main(String[] args) {
         int n = 0;
         try {
-            if(args.length == 1) {
+            if (args.length == 1) {
                 n = Integer.parseInt(args[0]);
             } else {
                 //read from console
@@ -18,7 +29,7 @@ public class SiebDesEratosthenes {
                 n = Integer.parseInt(scanner.next());
             }
 
-            if(n < 2) throw new IllegalArgumentException("illegal input");
+            if (n < 2) throw new IllegalArgumentException("illegal input");
         } catch (Exception e) {
             System.out.println("Falsche Eingabeparameter - Bitte eine Ganzzahl (>=2) eingeben");
             System.exit(1);
@@ -27,28 +38,14 @@ public class SiebDesEratosthenes {
         SiebDesEratosthenes sde = new SiebDesEratosthenes(n);
     }
 
-
-
-    public SiebDesEratosthenes(int n) {
-        Worker test = new Worker(n, 2);
-        test.start();
-        for(int i = 2; i <= n+1; i++){
-            test.values.add(i);
-        }
-        test.values.parentStopped = true;
-
-    }
-
-
-
-    private class Worker extends Thread{
+    private class Worker extends Thread {
+        Pipeline values;
         private Worker child;
         private int n;
         private int prime;
-        Pipeline values;
 
 
-        Worker(int n, int prime){
+        Worker(int n, int prime) {
             this.n = n;
             this.prime = prime;
             values = new Pipeline();
@@ -57,16 +54,16 @@ public class SiebDesEratosthenes {
 
         @Override
         public void run() {
-            while(!values.parentStopped || !values.isEmpty()){
+            while (!values.parentStopped || !values.isEmpty()) {
                 this.checkValue();
             }
 
-            System.out.print( prime + ", ");
+            System.out.print(prime + ", ");
 
-            if(child != null){
+            if (child != null) {
 
                 child.values.parentStopped = true;
-                synchronized (child.values){
+                synchronized (child.values) {
                     child.values.notifyAll();
                 }
             }
@@ -74,27 +71,27 @@ public class SiebDesEratosthenes {
 
 
         private void checkValue() {
-            int k =values.getNext();
+            int k = values.getNext();
 
-            if(k <= n && k != -1) {
+            if (k <= n && k != -1) {
                 if (k % prime != 0) {
                     if (child == null) {
-                     //   System.out.println("child spawned: " + k);
+                        //   System.out.println("child spawned: " + k);
                         child = new Worker(n, k);
                         child.start();
                     } else {//(Math.pow(k, 2) < n)
                         child.values.add(k);
                     }
                 }
-            }else{
+            } else {
                 this.end();
             }
         }
 
-        void end(){
+        void end() {
 
-            if(child != null){
-                synchronized (child.values){
+            if (child != null) {
+                synchronized (child.values) {
                     child.values.parentStopped = true;
                     child.values.notifyAll();
                 }
@@ -103,31 +100,31 @@ public class SiebDesEratosthenes {
 
     }
 
-    private class Pipeline{
-        private ArrayDeque<Integer> elements = new ArrayDeque<>();
+    private class Pipeline {
         public boolean parentStopped = false;
+        private ArrayDeque<Integer> elements = new ArrayDeque<>();
 
-        synchronized int getNext(){
-            while(elements.isEmpty() && !parentStopped ){
+        synchronized int getNext() {
+            while (elements.isEmpty() && !parentStopped) {
                 try {
                     wait();
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            if(!elements.isEmpty()){
+            if (!elements.isEmpty()) {
                 return elements.poll();
             } else {
-              return -1;
+                return -1;
             }
         }
 
-        synchronized void add(int newVal){
+        synchronized void add(int newVal) {
             elements.add(newVal);
             notifyAll();
         }
 
-        boolean isEmpty(){
+        boolean isEmpty() {
             return elements.isEmpty();
         }
 
