b9ed0417fae853a9e85c217f1dbd433f5cfc2ed7
Jonas Einig <jonas.einig@web.de>
2017-02-24 12:34:01 +0100

    first try but buggy

--- src/main/java/MatrixMultiplicator.java
+++ src/main/java/MatrixMultiplicator.java
@@ -10,25 +10,79 @@ import java.util.concurrent.TimeUnit;
 public class MatrixMultiplicator {
 
     public static void main(String[] args) {
-
+        double [][] a = {{ 1, 2, 3, 4, 5},
+                         { 6, 7, 8, 9, 10},
+                         { 1, 2, 3, 4, 5},
+                         { 6, 7, 8, 9, 10},
+                         { 1, 2, 3, 4, 5} };
+        new MatrixMultiplicator(a);
     }
 
-    private double[][] matrix;
+    private final double[][] result;
+    private final double[][] matrix;
 
     public MatrixMultiplicator(double [][] matrix){
+        assert(matrix != null);
+        assert(matrix.length == matrix[0].length);
+        assert(0 < matrix.length && matrix.length < 10 );
+
         this.matrix = matrix;
+        this.result = new double[matrix.length][matrix.length];
 
         ArrayBlockingQueue<Runnable> tasks = new ArrayBlockingQueue<Runnable>(matrix.length * matrix.length);
 
 
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = 0; j < matrix.length; j++)
+                tasks.add(new Multiplicator( i, j, matrix.length));
 
 
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor( 4, 8, 0, TimeUnit.SECONDS, tasks);
 
+        while(tpe.getTaskCount() > 0){
 
+        }
+        try {
+            tpe.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        prettyPrintMatrix(result);
 
-        ThreadPoolExecutor tpe = new ThreadPoolExecutor(matrix.length * matrix.length,matrix.length * matrix.length,0, TimeUnit.SECONDS, tasks);
     }
 
+    private void prettyPrintMatrix(double[][] data) {
+        for(int j = 0; j < matrix.length; j++) {
+            for (int i = 0; i < matrix.length; i++)
+                System.out.print(data[i][j] + ", ");
+
+            System.out.println();
+        }
+    }
+
+    private class Multiplicator implements Runnable {
+
+        private final int i;
+        private final int j;
+        private final int n;
+
+        public Multiplicator(int i, int j, int n) {
+            this.i = i;
+            this.j = j;
+            this.n = n;
+        }
+
+        @Override
+        public void run() {
+            double res = 0;
+            for(int k = 0; k < n; k++){
+                res += matrix[i][k] * matrix[k][j];
+            }
+            synchronized (result){
+                result[i][j] = res;
+            }
+        }
+    }
 
 
 
