affcc82c2dd7410583eb0b82aff1e7ddd696a3cd
Jonas Einig <jonas.einig@web.de>
2017-02-27 12:32:33 +0100

    initial work for nr 2

---
+++ matrixManyThread.c
@@ -0,0 +1,59 @@
+#define _POSIX_C_SOURCE 200112L
+//Johannes Hartmann, Jonas Einig
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define MATRIX_SIZE 5
+typedef struct task task;
+
+struct task {
+    double matrix[MATRIX_SIZE][MATRIX_SIZE];
+    double result;
+    int i;
+    int j;
+};
+
+void *perform_work(void *argument)
+{
+    printf("new thread running\n");
+    return NULL;
+}
+
+int main(int argc, char **argv)
+{
+
+    double matrix[MATRIX_SIZE][MATRIX_SIZE] =
+    {   1, 2, 3, 4, 5,
+        6, 7, 8, 9, 10,
+        1, 2, 3, 4, 5,
+        6, 7, 8, 9, 10,
+        1, 2, 3, 4, 5
+    };
+
+    pthread_t threads[ MATRIX_SIZE * MATRIX_SIZE ];
+
+    struct task tasklist[ MATRIX_SIZE * MATRIX_SIZE] = { NULL };
+
+
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            task tmp = {.matrix = matrix,  .i = i, .j = j};
+            // tasklist[(i * MATRIX_SIZE) + j].matrix = matrix;
+
+
+        }
+    }
+
+
+    //TODO: how many tasks and threads
+
+    pthread_create(&threads[1], NULL, perform_work, NULL);
+
+    // wait for thread to complete
+    // block until thread 'index' completes
+    pthread_join(threads[ 1 ], NULL);
+
+    printf("In main: All threads completed successfully\n");
+    exit(EXIT_SUCCESS);
+}
