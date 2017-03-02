e46dc0a1b52fdf41fc9a20674a9fb714132dbb1e
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-03-02 17:29:19 +0100

    done stuff

--- ipcsignal.c
+++ ipcsignal.c
@@ -10,6 +10,7 @@ void sendChar(char input, pid_t parentid);
 void sendBit(int bit, pid_t parentid);
 void reciverSignalHandler(int sigNumber);
 void senderSignalHandler(int sigNumber);
+void error(char const *errorMassage);
 
 
 // Elternvariablen
@@ -25,24 +26,19 @@ volatile int receiverReady = 0;
 int main(int argc, char **argv)
 {
     if ((pid = fork()) < 0) {
-        fprintf(stderr, "Fehler... \n");
+        error("should not happen");
     } else if (pid == 0) {
         /* Kindprozess */
-        if (signal(SIGUSR1, senderSignalHandler) == SIG_ERR) {
-            perror("Fehler beim setzen der Signalhandler");
-            exit(EXIT_FAILURE);
-        }
+        if (signal(SIGUSR1, senderSignalHandler) == SIG_ERR)
+            error("Fehler beim setzen der Signalhandler");
         pid_t parentid = getppid();
 
         char *input = (char *) malloc(sizeof(char) * 101);
         while (receiverReady == 0);
         printf("Bitte geben Sie eine Zeichenkette < 100 Zeichen ein:\n");
-        if (fgets(input, 101, stdin) == NULL) {
-            perror("Fehler beim lesen des Inputs");
-            exit(EXIT_FAILURE);
-        }
+        if (fgets(input, 100, stdin) == NULL)
+            error("Fehler beim lesen des Inputs");
         for (int i = 0; i < 100; i++) {
-            printf("sended char %c", input[i]);
             sendChar(input[i], parentid);
             if (input[i] == '\0') {
                 break;
@@ -50,26 +46,22 @@ int main(int argc, char **argv)
         }
     } else {
         /* Elternprozess */
-        if (signal(SIGUSR1, reciverSignalHandler) == SIG_ERR) {
-            perror("Fehler beim setzen der Signalhandler");
-            exit(EXIT_FAILURE);
-        }
-        if (signal(SIGUSR2, reciverSignalHandler) == SIG_ERR) {
-            perror("Fehler beim setzen der Signalhandler");
-            exit(EXIT_FAILURE);
-        }
-        sleep(1);
-        if (kill(pid, SIGUSR1) < 0) {
-            perror("Fehler beim senden von der bereitschaft");
-            exit(EXIT_FAILURE);
-        }
+        if (signal(SIGUSR1, reciverSignalHandler) == SIG_ERR)
+            error("Fehler beim setzen der Signalhandler");
+        if (signal(SIGUSR2, reciverSignalHandler) == SIG_ERR)
+            error("Fehler beim setzen der Signalhandler");
+        // give the child process some time to make signalhandler ;) 
+        sleep(0.1);
+        if (kill(pid, SIGUSR1) < 0)
+            error("Fehler beim senden von der bereitschaft");
         while (finished == 0);
         printf("Received result %s", resultString);
 
     }
 }
 
-void sendChar(char input, pid_t parentid) {
+void sendChar(char input, pid_t parentid)
+{
     for (int i = 0; i < 8; i++) {
         // wait for ack
         while (receiverReady == 0);
@@ -85,39 +77,29 @@ void sendChar(char input, pid_t parentid) {
 
 void senderSignalHandler(int sigNumber)
 {
-    printf("handled ack\n");
     if (sigNumber == SIGUSR1) {
         receiverReady = 1;
-    } else {
-        perror("Fehler beim empfangen der Signale");
-        exit(EXIT_FAILURE);
-    }
-    if (signal(SIGUSR1, senderSignalHandler) == SIG_ERR) {
-        perror("Fehler beim setzen der Signalhandler");
-        exit(EXIT_FAILURE);
-    }
+    } else
+        error("Fehler beim empfangen der Signale");
+    if (signal(SIGUSR1, senderSignalHandler) == SIG_ERR)
+        error("Fehler beim setzen der Signalhandler");
 }
 
 void reciverSignalHandler(int sigNumber)
 {
     if (sigNumber == SIGUSR1) {
-        received = (received << 1);
         signalcount ++;
-        if (signal(SIGUSR1, reciverSignalHandler) == SIG_ERR) {
-            perror("Fehler beim setzen der Signalhandler");
-            exit(EXIT_FAILURE);
-        }
+        if (signal(SIGUSR1, reciverSignalHandler) == SIG_ERR)
+            error("Fehler beim setzen der Signalhandler");
     } else if (sigNumber == SIGUSR2) {
         received = received | 1 << (signalcount % 8);
         signalcount ++;
-        if (signal(SIGUSR2, reciverSignalHandler) == SIG_ERR) {
-            perror("Fehler beim setzen der Signalhandler");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        perror("Fehler beim empfangen der Signale");
-        exit(EXIT_FAILURE);
-    }
+        if (signal(SIGUSR2, reciverSignalHandler) == SIG_ERR)
+            error("Fehler beim setzen der Signalhandler");
+    } else
+        error("Fehler beim empfangen der Signale");
+
+
     if ((signalcount % 8) == 0) {
         if (received == '\0') {
             finished = 1;
@@ -125,26 +107,23 @@ void reciverSignalHandler(int sigNumber)
         resultString[div(signalcount, 8).quot - 1] = received;
         received = '\0';
     }
-    if (kill(pid, SIGUSR1) < 0) {
-        perror("Fehler beim senden von der bereitschaft");
-        exit(EXIT_FAILURE);
-    }
+    if (kill(pid, SIGUSR1) < 0)
+        error("Fehler beim senden von der bereitschaft");
 }
 
 void sendBit(int bit, pid_t parentid)
 {
     if (bit == 0) {
-        if (kill(parentid, SIGUSR1) < 0) {
-            perror("Fehler beim senden von einem Bit");
-            exit(EXIT_FAILURE);
-        }
-        printf("sent 0");
+        if (kill(parentid, SIGUSR1) < 0)
+            error("Fehler beim senden von einem Bit");
     } else {
-        if (kill(parentid, SIGUSR2) < 0) {
-            perror("Fehler beim senden von einem Bit");
-            exit(EXIT_FAILURE);
-        }
-        printf("sent 1");
+        if (kill(parentid, SIGUSR2) < 0)
+            error("Fehler beim senden von einem Bit");
     }
     receiverReady = 0;
+}
+
+void error(char const *errorMassage) {
+    perror(errorMassage);
+    exit(EXIT_FAILURE);
 }
